672f480b9557f64f1c721255b4568b8483d7ce49
Shaurya Jain <shauryaalok@gmail.com>
2026-05-07 12:35:05 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ques = "";
+        for(auto x:s){
+            if((x <='z' && x >='a') || (x <= 'Z' && x >= 'A') || (x <= '9' && x >= '0')){
+                ques += tolower(x);
+            }
+        }
+
+        int n = ques.size();
+
+        for(int i = 0; i < n/2; i++){
+            if(ques[i] != ques[n-i-1]){
+                return false;
+            }
+        } 
+
+        return true;
+    }
+};
