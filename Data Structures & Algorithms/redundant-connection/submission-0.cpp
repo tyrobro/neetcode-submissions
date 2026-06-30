34847019f7dce6d0667ee710a50f35557c9da056
Shaurya Jain <shauryaalok@gmail.com>
2026-06-30 10:58:59 +0530

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<bool> visit;
+    vector<vector<int>> adj;
+    unordered_set<int> cycle;
+    int cycleStart;
+
+    bool dfs(int node, int par){
+        if(visit[node]){
+            cycleStart = node;
+            return true;
+        }
+
+        visit[node] = true;
+        for(int nei: adj[node]){
+            if(nei == par) continue;
+            if(dfs(nei, node)) {
+                if(cycleStart != -1) cycle.insert(node);
+                if(node == cycleStart) {
+                    cycleStart = -1;
+                }
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        adj.resize(n+1);
+        
+        for(auto& edge: edges){
+            int u = edge[0], v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        visit.resize(n+1, false);
+        cycleStart = -1;
+        dfs(1, -1);
+
+        for(int i = n -1; i >= 0 ; i--){
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            if(cycle.count(u) && cycle.count(v)){
+                return {u, v};
+            }
+        }
+        return {};
+    }
+};
