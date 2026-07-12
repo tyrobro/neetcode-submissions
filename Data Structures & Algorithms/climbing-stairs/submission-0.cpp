dd6aaf02dd219dbbf315b151464d04d11c9eed76
Shaurya Jain <shauryaalok@gmail.com>
2026-07-12 11:43:54 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int first = 1;
+        int second = 1;
+
+        for(int i = 0; i < n-1; i++){
+            int temp = first;
+            first = first+second;
+            second = temp;
+        }
+
+        return first;
+    }
+};
