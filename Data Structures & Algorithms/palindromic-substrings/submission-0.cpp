7ff86e373afb0247a6222bf79f59610183db1d88
Shaurya Jain <shauryaalok@gmail.com>
2026-07-17 08:26:20 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> manacher(string& s) {
+        if (!s.size()) return {};
+        string t = "#" + string(1, s[0]);
+        for (int i = 1; i < s.size(); ++i)
+            t += "#" + string(1, s[i]);
+        t += "#";
+        int n = t.size();
+        vector<int> p(n, 0);
+        int l = 0, r = 0;
+        for (int i = 0; i < n; i++) {
+            p[i] = (i < r) ? min(r - i, p[l + (r - i)]) : 0;
+            while (i + p[i] + 1 < n && i - p[i] - 1 >= 0 &&
+                   t[i + p[i] + 1] == t[i - p[i] - 1])
+                p[i]++;
+            if (i + p[i] > r)
+                l = i - p[i], r = i + p[i];
+        }
+        return p;
+    }
+
+    int countSubstrings(string s) {
+        vector<int> p = manacher(s);
+        int res = 0;
+        for (int i : p) {
+            res += (i + 1) / 2;
+        }
+        return res;
+    }
+};
