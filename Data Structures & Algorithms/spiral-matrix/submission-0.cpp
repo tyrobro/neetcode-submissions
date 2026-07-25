23013605b2d16c329a27d43b37caac6a6f69d4e0
Shaurya Jain <shauryaalok@gmail.com>
2026-07-25 14:12:31 +0530

    Add: spiral-matrix - submission-0

---
+++ Data Structures & Algorithms/spiral-matrix/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        vector<int> steps = {(int)matrix[0].size(), (int)matrix.size()-1};
+
+        int r = 0, c= -1, d = 0;
+        while(steps[d%2]){
+            for(int i = 0; i < steps[d%2]; i++){
+                r += directions[d].first;
+                c += directions[d].second;
+                ans.push_back(matrix[r][c]);
+            }
+
+            steps[d%2]--;
+            d = (d+1)%4;
+        }
+
+        return ans;
+    }
+};
